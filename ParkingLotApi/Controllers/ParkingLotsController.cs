e5d6545cb3105bad4c9fd4f00ef424e3466fef9d
Shang Huang <shuang19@slb.com>
2023-11-08 14:28:27 +0800

    mend

---
+++ ParkingLotApi/Controllers/ParkingLotsController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using ParkingLotApi.Dtos;
+
+namespace ParkingLotApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParkingLotsController : ControllerBase
+    {
+        
+
+        [HttpPost]
+        public async Task<ActionResult<ParkingLotDto>> AddParkingLotAsync([FromBody] ParkingLotDto parkingLotDto)
+        {
+            if (parkingLotDto.Capacity < 10)
+            {
+                return BadRequest();
+            }
+            return null;
+        }
+    }
+}
