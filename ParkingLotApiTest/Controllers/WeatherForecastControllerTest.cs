8e368d9b348e939c95adfe92cc791b14c205c328
Xueqian Li <XLi112@slb.com>
2023-11-08 13:04:03 +0800

    test: test get all weatherForecast

---
+++ ParkingLotApiTest/Controllers/WeatherForecastControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using ParkingLotApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotApiTest.Controllers
+{
+    public class WeatherForecastControllerTest : TestBase
+    {
+        public WeatherForecastControllerTest(WebApplicationFactory<Program> webApplicationFactory) : base(webApplicationFactory)
+        {
+        }
+
+        [Fact]
+        public async Task Should_return_weatherForecast_list_when_getAll()
+        {
+            var httpResponseMessage = await GetClient().GetAsync("/WeatherForecast");
+            var weatherForecasts = await httpResponseMessage.Content.ReadFromJsonAsync<List<WeatherForecast>>();
+
+            Assert.NotNull(weatherForecasts);
+        }
+    }
+}
---
+++ ParkingLotApiTest/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotApiTest
+{
+    public class TestBase : IClassFixture<WebApplicationFactory<Program>>
+    {
+        protected WebApplicationFactory<Program> webApplicationFactory { get; }
+        
+        public TestBase(WebApplicationFactory<Program> webApplicationFactory) { 
+            this.webApplicationFactory = webApplicationFactory;
+        }
+        
+        protected HttpClient GetClient()
+        {
+            return webApplicationFactory.CreateClient();
+        }
+
+    }
+}
