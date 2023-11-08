87e887f23424a7f1d45ab6b30af26ab604a462de
Yaxin Yang <yyang66@slb.com>
2023-11-08 14:13:10 +0800

    feat: add create Parkinglot method

---
+++ ParkingLotApi/Controllers/ParkingLotsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using ParkingLotApi.Dtos;
+
+namespace ParkingLotApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParkingLotsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<ParkingLot>> CreateParkingLot(ParkingLotDto parkingLotDto)
+        {
+            if (parkingLotDto == null || parkingLotDto.Capacity <10)
+            {
+                return BadRequest();
+            }
+            return null;
+        }
+    }
+}
---
+++ ParkingLotApi/Dtos/ParkingLot.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkingLotApi.Dtos
+{
+    public class ParkingLot
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string Location { get; set; }
+    }
+}
