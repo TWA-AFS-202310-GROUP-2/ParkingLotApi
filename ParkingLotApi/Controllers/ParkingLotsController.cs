f47b9ddd01e6fb7192c790f63e8316c6a2e82cd4
Yaxin Yang <yyang66@slb.com>
2023-11-08 19:11:52 +0800

    feat: Ac3: add the pageindex get method

--- ParkingLotApi/Controllers/ParkingLotsController.cs
+++ ParkingLotApi/Controllers/ParkingLotsController.cs
@@ -28,5 +28,16 @@ namespace ParkingLotApi.Controllers
         {
             return await _parkingLotservices.GetByIdAsync(id);
         }
+        [HttpGet("{pageIndex}/{pageSize}")]
+        public async Task<ActionResult<List<ParkingLot>>> GetByPageAsync(int pageIndex, int pageSize)
+        {
+            //List<ParkingLot> parkingLots = new List<ParkingLot>();
+            for (int i = 1; i < 100; i++)
+            {
+                ParkingLotDto parkingLotDto = new ParkingLotDto { Name = $"Parking Lot {i}", Capacity = 50, Location = $"Location {i}" };
+                await _parkingLotservices.CreateAsync(parkingLotDto);
+            }
+            return StatusCode(StatusCodes.Status200OK, await _parkingLotservices.GetByPageAsync(pageIndex, pageSize));
+        }
     }
 }
