721be326d1eec1af0720bd026c7464c06ab0ecdb
Zliu53 <ZLiu53@slb.com>
2023-11-08 13:04:14 +0800

    Test: add WeatherForecast Get Test

---
+++ ParkingLotApiTest/NewFolder/WeatherForecastControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using ParkingLotApi;
+
+namespace ParkingLotApiTest.NewFolder
+{
+    public class WeatherForecastControllerTest
+    {
+        private HttpClient _httpClient;
+        public WeatherForecastControllerTest()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task TestResult_ReturnSucceed_GetWeatherForecasts()
+        {
+            var response = await _httpClient.GetAsync("/WeatherForecast");
+            var WeatherForecastList = await response.Content.ReadFromJsonAsync<List<WeatherForecast>>();
+            Assert.Equal(HttpStatusCode.OK,response.StatusCode);
+            Assert.Equal(5,WeatherForecastList.Count);
+        }
+    }
+}
