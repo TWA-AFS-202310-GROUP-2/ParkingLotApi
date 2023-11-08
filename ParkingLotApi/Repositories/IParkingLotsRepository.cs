54d12837ef5d375d19183e5bc06ce7e01aad75e0
Yiyang Zhang <YZhang184@slb.com>
2023-11-08 17:16:37 +0800

    feat: connect MongoDB and create parkingLot function

---
+++ ParkingLotApi/Repositories/IParkingLotsRepository.cs
@@ -0,0 +1,10 @@
+﻿using ParkingLotApi.Models;
+
+namespace ParkingLotApi.Repositories
+{
+    public interface IParkingLotsRepository
+    {
+        public Task<ParkingLot> CreateParkingLot(ParkingLot parkingLot);
+
+    }
+}
